982a2d7145cbb2ba63816ebf0a80dc29d8362ffd
happyew <645339328@qq.com>
2021-08-17 18:40:57 +0800

    更改点击验证码图片的js

--- src/main/java/com/example/shirodemo/util/SaltUtil.java
+++ src/main/java/com/example/shirodemo/util/SaltUtil.java
@@ -7,7 +7,7 @@ import org.apache.shiro.crypto.SecureRandomNumberGenerator;
  * 盐工具类
  */
 public class SaltUtil {
-    private static final char[] CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ~%^*()_-+0123456789".toCharArray();
+//    private static final char[] CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ~%^*()_-+0123456789".toCharArray();
 
     public static String getSalt() {
 //        StringBuilder sb = new StringBuilder();
